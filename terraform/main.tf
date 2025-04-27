682a0017ff6947e90b027715438b9a33ab2d5429
Marian Ferenc <mfrenec@ngncc.de>
2025-04-27 17:08:27 +0200

    wip impl

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,9 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
   
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   enable_nat_gateway = false
   
   tags = var.common_tags
